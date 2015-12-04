a5f20a222a8f5572040e3755bfb1a00e6220e609
Tom Denley <tsdenley@gmail.com>
2015-12-04 12:30:27 +0000

    Showing page template/route

--- common/routing.js
+++ common/routing.js
@@ -4,7 +4,7 @@
  * ReactionCore common/routing.js contains the core routes.
  */
 
-let staticPages = ["about", "team", "faqs", "terms", "privacy", "newPage" ];
+let staticPages = [ 'showing', 'prints', 'artists' ];
 
 /**
  * app router mapping
@@ -16,6 +16,22 @@ Router.map(function route() {
       name: page
     });
   }
+
+  // TODO: replace home route
+	// this.route("home", {
+  //   controller: ShopController,
+  //   path: "/",
+  //   name: "home",
+  //   template: "products",
+	// 	data: {
+	// 		test: true
+	// 	},
+  //   waitOn: function () {
+  //     return this.subscribe("Products", Session.get("productScrollLimit"));
+  //     console.log(this.data);
+  //   }
+  // });
+
   return this.route("notFound", {
     path: "/(.*)"
   });
