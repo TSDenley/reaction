303ff6780ae409ea9f78d0736c643213e19b3f50
Tom Denley <tsdenley@gmail.com>
2015-12-04 18:00:37 +0000

    Updated show tag ID

--- common/routing.js
+++ common/routing.js
@@ -21,7 +21,7 @@ Router.map(function route() {
   _.extend(Router.routes.index.options, {
     template: 'homepage',
     data: function() {
-      const showTagId = '6SzYLFCbQvh7WduCy';
+      const showTagId = 'tm6ZME5p8ze28yoe7';
 
       return {
         showTag: Tags.findOne({ _id: showTagId }),
