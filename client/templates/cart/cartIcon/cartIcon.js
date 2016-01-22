04b30dbd0fd2dec45f8e47fb243ecd64a1798dd7
Tom Denley <tsdenley@gmail.com>
2016-01-22 15:13:51 +0000

    Added scroll to top when toggling cart

--- client/templates/cart/cartIcon/cartIcon.js
+++ client/templates/cart/cartIcon/cartIcon.js
@@ -1,1 +1,10 @@
 Template.newCartIcon.replaces('cartIcon');
+
+/**
+* Add scroll to top when opening cart
+*/
+Template.cartIcon.events({
+  "click .show-cart": function () {
+		$('html, body').animate({ scrollTop : 0 }, 200);
+  }
+});
